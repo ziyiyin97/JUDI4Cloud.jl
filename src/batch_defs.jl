e3194529a1f857334795ad171a41fc6e20f420aa
Ziyi (Francis) Yin <ziyi.yin@gatech.edu>
2021-09-23 22:14:03 -0400

    give back broadcasting

--- src/batch_defs.jl
+++ src/batch_defs.jl
@@ -25,8 +25,10 @@ end
 end
 
 #LSRTM
-@batchdef function lsrtm_objective_azure(model, q, D, dm, opt; nlind=false)
+@batchdef function lsrtm_objective_azure(_model::BatchFuture, q, D, _dm, opt; nlind=false)
     check_njulia(q.nsrc)
+    model = fetch(_model)
+    dm = isnothing(_dm) ? _dm : fetch(_dm)
     f, g = lsrtm_objective(model,q, D, dm; options=opt, nlind=nlind)
     rmprocs(workers())
     return f, g
